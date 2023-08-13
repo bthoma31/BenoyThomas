5472f01b57e0b26b349064eb8be7d075ade1360a
bthoma31 <122180199+bthoma31@users.noreply.github.com>
2023-08-13 18:56:50 -0400

    Delete Password Genertor.py

--- Password Genertor.py
+++
@@ -1,9 +0,0 @@
-import random
-print('Your Password: ')
-chars='abcdefghijklmnopqrstuvwxyz1234567890!@#$%'
-
-password=''
-for x in range(8):
-     password +=random.choice(chars)
-
-print(password)
